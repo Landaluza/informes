46d4912361acc75d35da966818d3aebb035ee4c5
Jacinto <calidad@landaluza.es>
2014-07-25 12:38:48 +0200

    Informe inicial para el Vinagre de Jerez

---
+++ VinagreJerezResumen/informe/Form1.Designer.vb
@@ -0,0 +1,191 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim ReportDataSource1 As Microsoft.Reporting.WinForms.ReportDataSource = New Microsoft.Reporting.WinForms.ReportDataSource()
+        Dim ReportDataSource2 As Microsoft.Reporting.WinForms.ReportDataSource = New Microsoft.Reporting.WinForms.ReportDataSource()
+        Dim ReportDataSource3 As Microsoft.Reporting.WinForms.ReportDataSource = New Microsoft.Reporting.WinForms.ReportDataSource()
+        Dim ReportDataSource4 As Microsoft.Reporting.WinForms.ReportDataSource = New Microsoft.Reporting.WinForms.ReportDataSource()
+        Dim ReportDataSource5 As Microsoft.Reporting.WinForms.ReportDataSource = New Microsoft.Reporting.WinForms.ReportDataSource()
+        Dim ReportDataSource6 As Microsoft.Reporting.WinForms.ReportDataSource = New Microsoft.Reporting.WinForms.ReportDataSource()
+        Dim ReportDataSource7 As Microsoft.Reporting.WinForms.ReportDataSource = New Microsoft.Reporting.WinForms.ReportDataSource()
+        Me.ReportViewer1 = New Microsoft.Reporting.WinForms.ReportViewer()
+        Me.ResumenExpedicionEmbotelladosBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.LADataSet = New informe.LADataSet()
+        Me.ResumenExpedicionGranelBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.ResumenUsadoIngredienteBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.DetalleExpedicionesEmbotelladoBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.ResumenComprasMateriasPrimasBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.ResumenEmbotelladosBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.ResumenExpedicionEmbotelladosTableAdapter = New informe.LADataSetTableAdapters.ResumenExpedicionEmbotelladosTableAdapter()
+        Me.ResumenExpedicionGranelTableAdapter = New informe.LADataSetTableAdapters.ResumenExpedicionGranelTableAdapter()
+        Me.ResumenUsadoIngredienteTableAdapter = New informe.LADataSetTableAdapters.ResumenUsadoIngredienteTableAdapter()
+        Me.DetalleExpedicionesEmbotelladoTableAdapter = New informe.LADataSetTableAdapters.DetalleExpedicionesEmbotelladoTableAdapter()
+        Me.ResumenComprasMateriasPrimasTableAdapter = New informe.LADataSetTableAdapters.ResumenComprasMateriasPrimasTableAdapter()
+        Me.ResumenEmbotelladosTableAdapter = New informe.LADataSetTableAdapters.ResumenEmbotelladosTableAdapter()
+        Me.ResumenEmbotelladosIncluyePicosBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.ResumenEmbotelladosIncluyePicosTableAdapter = New informe.LADataSetTableAdapters.ResumenEmbotelladosIncluyePicosTableAdapter()
+        CType(Me.ResumenExpedicionEmbotelladosBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.LADataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ResumenExpedicionGranelBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ResumenUsadoIngredienteBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DetalleExpedicionesEmbotelladoBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ResumenComprasMateriasPrimasBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ResumenEmbotelladosBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ResumenEmbotelladosIncluyePicosBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ReportViewer1
+        '
+        Me.ReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill
+        ReportDataSource1.Name = "ResumenExpedicionEmbotellados"
+        ReportDataSource1.Value = Me.ResumenExpedicionEmbotelladosBindingSource
+        ReportDataSource2.Name = "ResumenExpedicionGranel"
+        ReportDataSource2.Value = Me.ResumenExpedicionGranelBindingSource
+        ReportDataSource3.Name = "ResumenUsadoIngrediente"
+        ReportDataSource3.Value = Me.ResumenUsadoIngredienteBindingSource
+        ReportDataSource4.Name = "DetalleExpedicionesEmbotellado"
+        ReportDataSource4.Value = Me.DetalleExpedicionesEmbotelladoBindingSource
+        ReportDataSource5.Name = "ResumenComprasMateriasPrimas"
+        ReportDataSource5.Value = Me.ResumenComprasMateriasPrimasBindingSource
+        ReportDataSource6.Name = "ResumenEmbotellados"
+        ReportDataSource6.Value = Me.ResumenEmbotelladosBindingSource
+        ReportDataSource7.Name = "ResumenEmbotelladosIncluyePicos"
+        ReportDataSource7.Value = Me.ResumenEmbotelladosIncluyePicosBindingSource
+        Me.ReportViewer1.LocalReport.DataSources.Add(ReportDataSource1)
+        Me.ReportViewer1.LocalReport.DataSources.Add(ReportDataSource2)
+        Me.ReportViewer1.LocalReport.DataSources.Add(ReportDataSource3)
+        Me.ReportViewer1.LocalReport.DataSources.Add(ReportDataSource4)
+        Me.ReportViewer1.LocalReport.DataSources.Add(ReportDataSource5)
+        Me.ReportViewer1.LocalReport.DataSources.Add(ReportDataSource6)
+        Me.ReportViewer1.LocalReport.DataSources.Add(ReportDataSource7)
+        Me.ReportViewer1.LocalReport.ReportEmbeddedResource = "informe.Report1.rdlc"
+        Me.ReportViewer1.Location = New System.Drawing.Point(0, 0)
+        Me.ReportViewer1.Name = "ReportViewer1"
+        Me.ReportViewer1.Size = New System.Drawing.Size(484, 386)
+        Me.ReportViewer1.TabIndex = 0
+        '
+        'ResumenExpedicionEmbotelladosBindingSource
+        '
+        Me.ResumenExpedicionEmbotelladosBindingSource.DataMember = "ResumenExpedicionEmbotellados"
+        Me.ResumenExpedicionEmbotelladosBindingSource.DataSource = Me.LADataSet
+        '
+        'LADataSet
+        '
+        Me.LADataSet.DataSetName = "LADataSet"
+        Me.LADataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'ResumenExpedicionGranelBindingSource
+        '
+        Me.ResumenExpedicionGranelBindingSource.DataMember = "ResumenExpedicionGranel"
+        Me.ResumenExpedicionGranelBindingSource.DataSource = Me.LADataSet
+        '
+        'ResumenUsadoIngredienteBindingSource
+        '
+        Me.ResumenUsadoIngredienteBindingSource.DataMember = "ResumenUsadoIngrediente"
+        Me.ResumenUsadoIngredienteBindingSource.DataSource = Me.LADataSet
+        '
+        'DetalleExpedicionesEmbotelladoBindingSource
+        '
+        Me.DetalleExpedicionesEmbotelladoBindingSource.DataMember = "DetalleExpedicionesEmbotellado"
+        Me.DetalleExpedicionesEmbotelladoBindingSource.DataSource = Me.LADataSet
+        '
+        'ResumenComprasMateriasPrimasBindingSource
+        '
+        Me.ResumenComprasMateriasPrimasBindingSource.DataMember = "ResumenComprasMateriasPrimas"
+        Me.ResumenComprasMateriasPrimasBindingSource.DataSource = Me.LADataSet
+        '
+        'ResumenEmbotelladosBindingSource
+        '
+        Me.ResumenEmbotelladosBindingSource.DataMember = "ResumenEmbotellados"
+        Me.ResumenEmbotelladosBindingSource.DataSource = Me.LADataSet
+        '
+        'ResumenExpedicionEmbotelladosTableAdapter
+        '
+        Me.ResumenExpedicionEmbotelladosTableAdapter.ClearBeforeFill = True
+        '
+        'ResumenExpedicionGranelTableAdapter
+        '
+        Me.ResumenExpedicionGranelTableAdapter.ClearBeforeFill = True
+        '
+        'ResumenUsadoIngredienteTableAdapter
+        '
+        Me.ResumenUsadoIngredienteTableAdapter.ClearBeforeFill = True
+        '
+        'DetalleExpedicionesEmbotelladoTableAdapter
+        '
+        Me.DetalleExpedicionesEmbotelladoTableAdapter.ClearBeforeFill = True
+        '
+        'ResumenComprasMateriasPrimasTableAdapter
+        '
+        Me.ResumenComprasMateriasPrimasTableAdapter.ClearBeforeFill = True
+        '
+        'ResumenEmbotelladosTableAdapter
+        '
+        Me.ResumenEmbotelladosTableAdapter.ClearBeforeFill = True
+        '
+        'ResumenEmbotelladosIncluyePicosBindingSource
+        '
+        Me.ResumenEmbotelladosIncluyePicosBindingSource.DataMember = "ResumenEmbotelladosIncluyePicos"
+        Me.ResumenEmbotelladosIncluyePicosBindingSource.DataSource = Me.LADataSet
+        '
+        'ResumenEmbotelladosIncluyePicosTableAdapter
+        '
+        Me.ResumenEmbotelladosIncluyePicosTableAdapter.ClearBeforeFill = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(484, 386)
+        Me.Controls.Add(Me.ReportViewer1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        CType(Me.ResumenExpedicionEmbotelladosBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.LADataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ResumenExpedicionGranelBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ResumenUsadoIngredienteBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DetalleExpedicionesEmbotelladoBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ResumenComprasMateriasPrimasBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ResumenEmbotelladosBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ResumenEmbotelladosIncluyePicosBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Private WithEvents ReportViewer1 As Microsoft.Reporting.WinForms.ReportViewer
+    Friend WithEvents ResumenExpedicionEmbotelladosBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents LADataSet As informe.LADataSet
+    Friend WithEvents ResumenExpedicionGranelBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents ResumenUsadoIngredienteBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents DetalleExpedicionesEmbotelladoBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents ResumenExpedicionEmbotelladosTableAdapter As informe.LADataSetTableAdapters.ResumenExpedicionEmbotelladosTableAdapter
+    Friend WithEvents ResumenExpedicionGranelTableAdapter As informe.LADataSetTableAdapters.ResumenExpedicionGranelTableAdapter
+    Friend WithEvents ResumenUsadoIngredienteTableAdapter As informe.LADataSetTableAdapters.ResumenUsadoIngredienteTableAdapter
+    Friend WithEvents DetalleExpedicionesEmbotelladoTableAdapter As informe.LADataSetTableAdapters.DetalleExpedicionesEmbotelladoTableAdapter
+    Friend WithEvents ResumenComprasMateriasPrimasBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents ResumenComprasMateriasPrimasTableAdapter As informe.LADataSetTableAdapters.ResumenComprasMateriasPrimasTableAdapter
+    Friend WithEvents ResumenEmbotelladosBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents ResumenEmbotelladosTableAdapter As informe.LADataSetTableAdapters.ResumenEmbotelladosTableAdapter
+    Friend WithEvents ResumenEmbotelladosIncluyePicosBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents ResumenEmbotelladosIncluyePicosTableAdapter As informe.LADataSetTableAdapters.ResumenEmbotelladosIncluyePicosTableAdapter
+
+End Class
---
+++ VinagreJerezResumen/informe/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'LADataSet.ResumenEmbotelladosIncluyePicos' table. You can move, or remove it, as needed.
+        Me.ResumenEmbotelladosIncluyePicosTableAdapter.Fill(Me.LADataSet.ResumenEmbotelladosIncluyePicos, "01/07/2014", "23/07/2014")
+        Me.ResumenEmbotelladosTableAdapter.Fill(Me.LADataSet.ResumenEmbotellados, "01/07/2014", "23/07/2014")
+        'TODO: This line of code loads data into the 'LADataSet.ResumenComprasMateriasPrimas' table. You can move, or remove it, as needed.
+        Me.ResumenComprasMateriasPrimasTableAdapter.Fill(Me.LADataSet.ResumenComprasMateriasPrimas, "01/07/2014", "23/07/2014")
+        'TODO: This line of code loads data into the 'LADataSet.DetalleExpedicionesEmbotellado' table. You can move, or remove it, as needed.
+        Me.DetalleExpedicionesEmbotelladoTableAdapter.Fill(Me.LADataSet.DetalleExpedicionesEmbotellado, "01/07/2014", "23/07/2014")
+        'TODO: This line of code loads data into the 'LADataSet.ResumenUsadoIngrediente' table. You can move, or remove it, as needed.
+        Me.ResumenUsadoIngredienteTableAdapter.Fill(Me.LADataSet.ResumenUsadoIngrediente, "01/07/2014", "23/07/2014")
+        'TODO: This line of code loads data into the 'LADataSet.ResumenExpedicionEmbotellados' table. You can move, or remove it, as needed.
+        Me.ResumenExpedicionEmbotelladosTableAdapter.Fill(Me.LADataSet.ResumenExpedicionEmbotellados, "01/07/2014", "23/07/2014")
+        'TODO: This line of code loads data into the 'LADataSet.ResumenExpedicionGranel' table. You can move, or remove it, as needed.
+        Me.ResumenExpedicionGranelTableAdapter.Fill(Me.LADataSet.ResumenExpedicionGranel, "01/07/2014", "23/07/2014")
+
+        Me.ReportViewer1.RefreshReport()
+    End Sub
+
+End Class
