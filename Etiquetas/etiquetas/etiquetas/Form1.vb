9107c2a75e8b59a60727389695624cc8a94c742b
miguel_vega_menacho <miguel.vega.menacho@gmail.com>
2014-11-07 13:57:36 +0100

    no message

---
+++ Etiquetas/etiquetas/etiquetas/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'TODO: esta línea de código carga datos en la tabla 'LADataSet.EtiquetasPaletSelect' Puede moverla o quitarla según sea necesario.
+        ' Me.EtiquetasPaletSelectTableAdapter.Fill(Me.LADataSet.EtiquetasPaletSelect, 0)
+        Me.ReportViewer1.RefreshReport()
+    End Sub
+
+End Class
